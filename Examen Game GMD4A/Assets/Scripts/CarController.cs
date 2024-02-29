a66215620ce88b95b8df5491ad80e4bf31354563
Ruben-nn <rubenvdweg@gmail.com>
2024-02-29 12:48:27 +0100

    mapjes + input system

---
+++ Examen Game GMD4A/Assets/Scripts/CarController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Car car;
+
+    public void OnGasinput()
+    {
+
+    }
+    public void OnSteerInput()
+    {
+
+    }
+}
