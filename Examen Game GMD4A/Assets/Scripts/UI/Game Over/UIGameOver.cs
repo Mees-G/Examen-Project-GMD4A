8deb900955047f441901f8d7d111720cc23132ee
Mees-G <54513356+Mees-G@users.noreply.github.com>
2024-03-24 13:59:16 +0100

    Update UIGameOver.cs

--- Examen Game GMD4A/Assets/Scripts/UI/Game Over/UIGameOver.cs
+++ Examen Game GMD4A/Assets/Scripts/UI/Game Over/UIGameOver.cs
@@ -5,7 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class UIGameOver : MonoBehaviour
 {
-    
+
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        SelectedButtonOutlineManager.INSTANCE.input.UI.Enable();
+    }
+
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
